a48b6ac8111a1e803a0024edaaf2da8b21a66b92
gnanasundarsf <gnanasundarsf@gmail.com>
2019-10-23 21:19:55 +0530

    Update SalesforceProxy.ts
    update

--- src/salesforce/SalesforceProxy.ts
+++ src/salesforce/SalesforceProxy.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Report } from './../models/Report.interface';
 
-let proxyURL = 'hdialogflowvoice-developer-edition.na113.force.com/v/services/apexrest';
+let proxyURL = 'https://dialogflowvoice-developer-edition.na113.force.com/v/services/apexrest';
 
 export class SalesforceProxy {
 
